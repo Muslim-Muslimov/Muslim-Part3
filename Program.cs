5e896677fad0383ee37b73feafd1971c51de0c29
appl9 <appl9@DESKTOP-35T68GB>
2023-11-16 18:34:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Muslim_Part
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
