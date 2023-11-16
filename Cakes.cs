1fae4c79fc917ccb5e1f803bb8cb6655bc0ef90d
Муслим Марашев <appl95@bk.ru>
2023-11-16 18:44:16 +0300

    добавил метод Start

---
+++ Cakes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muslim_Part
+{
+    internal class Cakes
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
