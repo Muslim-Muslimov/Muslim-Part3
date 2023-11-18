692e5ba280f4bc66e4a137e68fb8a59fbbb8092c
Муслим Марашев <appl95@bk.ru>
2023-11-18 16:15:43 +0300

    Решил 2 задачу.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Привет мир!");
+            Console.WriteLine("Введите числа для сравнения: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            if (n1 < n2)
+                Console.WriteLine($"{n1} меньше {n2}");
+            else if (n1 > n2)
+                Console.WriteLine($"{n1} больше {n2}");
+            else
+                Console.WriteLine($"{n1} равно {n2}");
+            
         }
     }
 }
