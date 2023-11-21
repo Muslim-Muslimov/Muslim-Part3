e42ad3070a95f0a7b3f68778a29175dfab7d1c43
Муслим Марашев <appl95@bk.ru>
2023-11-21 13:36:09 +0300

    Создал файл

--- Program.cs
+++ Program.cs
@@ -4,15 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите числа для сравнения: ");
-            int n1 = Convert.ToInt32(Console.ReadLine());
-            int n2 = Convert.ToInt32(Console.ReadLine());
-            if (n1 < n2)
-                Console.WriteLine($"{n1} меньше {n2}");
-            else if (n1 > n2)
-                Console.WriteLine($"{n1} больше {n2}");
-            else
-                Console.WriteLine($"{n1} равно {n2}");
+            SravnenieDbuhChisel.Start();
             
         }
     }
